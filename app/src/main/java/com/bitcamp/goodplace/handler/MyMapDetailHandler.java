ae6d8fc8ffebf66a06284b0d5133a7953acb3e1f
eldehd9898 <eldehd9898@gmail.com>
2021-09-27 02:03:40 +0900

    refactor : Change the conditional statement to map.

--- app/src/main/java/com/bitcamp/goodplace/handler/MyMapDetailHandler.java
+++ app/src/main/java/com/bitcamp/goodplace/handler/MyMapDetailHandler.java
@@ -1,68 +1,64 @@
 package com.bitcamp.goodplace.handler;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.bitcamp.goodplace.domain.Place;
 import com.bitcamp.goodplace.domain.Theme;
 import com.bitcamp.goodplace.domain.User;
 import com.bitcamp.util.Prompt;
 
 public class MyMapDetailHandler extends AbstractMyMapHandler {
-	Map<String,String> controlThemeMenu = new HashMap<>();
-	
-	public MyMapDetailHandler(){
-		controlThemeMenu.put("pa", "/place/add");
-		controlThemeMenu.put("pd", "/place/delete");
-		controlThemeMenu.put("tu", "/myMap/update");
+	Map<String, Map<String, String>> controlMenu = new HashMap<>();
+
+	public MyMapDetailHandler() {
+		addPlaceDetailMapValue();
+		addThemeDetailMapValue();
+		controlMenu.put("이전 메뉴",null);
 	}
+
 	@Override
 	public void execute(CommandRequest request) throws Exception {
-		
-		List<Theme> currentTheme = AuthLoginHandler.getLoginUser().getThemeList();
 		Theme searchedTheme;
-		String input;
 		
 		System.out.println("[테마 상세보기]");
 		System.out.println();
-		
+
 		showThemeList();
 		searchedTheme = chooseTheme();
-		
+
 		User user = AuthLoginHandler.getLoginUser();
 		if (!user.getNickName().equals(searchedTheme.getUserName()) && user.getEmail().equals("root@test.com")) {
 			return;
 		}
 
 		request.setAttrubute("searchedTheme", searchedTheme);
-		
+
 		while (true) {
-			input =Prompt.inputString("지도 관리(1), 테마 관리(2), 이전 메뉴(0) ");
 			System.out.println();
-			if(Integer.parseInt(input) == 0) {
-				return;
-			} else if(Integer.parseInt(input) == 1) {
-				showPlaceList(searchedTheme);
-				input = Prompt.inputString("장소 추가(pa), 장소 삭제(pd), 이전메뉴(0)");
-				if(input.equals("0")) return;
-				request.getRequestDispatcher(controlThemeMenu.get(input.toLowerCase())).forword(request);
-				return;
-			} else if ( Integer.parseInt(input) == 2 ) {
-				input = Prompt.inputString("테마 수정(tu), 이전메뉴(0)");
-				if(input == "0") return;
-				request.getRequestDispatcher(controlThemeMenu.get(input.toLowerCase())).forword(request);
-				return;
-			} else {
-				System.out.println("명령어가 올바르지 않습니다!");
-				continue;
-			}
+			
+			ArrayList<String> controlMenuListOfKeys = new ArrayList<>(controlMenu.keySet());
+			Collections.swap(controlMenuListOfKeys, controlMenuListOfKeys.indexOf("이전 메뉴"), controlMenuListOfKeys.size()-1);
+			int selectedNum = chooseNum(controlMenuListOfKeys);
+
+			Map<String, String> detailMenu = controlMenu.get(controlMenuListOfKeys.get(selectedNum - 1));
+			if(detailMenu == null) return;
+			
+			ArrayList<String> detailMenuListOfKeys = new ArrayList<>(detailMenu.keySet());
+			selectedNum = chooseNum(detailMenuListOfKeys);
+			if(detailMenu.get(detailMenuListOfKeys.get(selectedNum-1)).equals("0")) return;
+			
+			request.getRequestDispatcher(detailMenu.get(detailMenuListOfKeys.get(selectedNum-1))).forword(request);
+			
 		}
-		
+
 	}
+
 	private void showThemeList() {
 		int index = 1;
-		for(Theme theme : AuthLoginHandler.getLoginUser().getThemeList()) {
+		for (Theme theme : AuthLoginHandler.getLoginUser().getThemeList()) {
 			System.out.printf("(%d)\n", index++);
 			System.out.printf("테마 제목 : %s\n", theme.getTitle());
 			System.out.printf("카테고리 : %s\n", theme.getCategory());
@@ -71,34 +67,59 @@ public class MyMapDetailHandler extends AbstractMyMapHandler {
 			System.out.println();
 		}
 	}
-	private void showPlaceList(Theme searchedTheme) {
-		int index = 1;
-		for (Place placeList : searchedTheme.getPlaceList()) {
-			System.out.printf("(%d)\n", index++);
-			System.out.printf("장소 이름 > %s\n", placeList.getStoreName());
-			System.out.printf("장소 주소 > %s\n", placeList.getAddressName());
-			System.out.printf("위도 > %s\n", placeList.getxCoord());
-			System.out.printf("경도 > %s\n", placeList.getyCoord());
-			System.out.printf("장소 후기 > %s\n", placeList.getComment().toString());
-			System.out.println();
-		}
-	}
+
 	private Theme chooseTheme() {
-		while(true) {
+		while (true) {
 			try {
 				int inputNum = Prompt.inputInt("번호를 입력하세요! ");
-				if(inputNum > AuthLoginHandler.getLoginUser().getThemeList().size() || inputNum < 0) {
+				if (inputNum > AuthLoginHandler.getLoginUser().getThemeList().size() || inputNum < 0) {
 					System.out.println("무효한 번호 입니다");
 					continue;
 				}
-				return AuthLoginHandler.getLoginUser().getThemeList().get(inputNum-1);
-			}catch (Exception e) {
+				return AuthLoginHandler.getLoginUser().getThemeList().get(inputNum - 1);
+			} catch (Exception e) {
 				System.out.println("------------------------------------------");
 				System.out.printf("오류 발생: %s\n", e.getClass().getName());
 				System.out.println("------------------------------------------");
 			}
-				
+
 		}
 	}
-	
+	private int chooseNum(List<String> list) {
+		int i = 1;
+		int selectedNum;
+		while(true) {
+			try {
+				for (String key : list) {
+					System.out.printf("%d. %s\n", i++, key);
+				}
+				
+				selectedNum = Prompt.inputInt("선택> ");
+				if(selectedNum > list.size() || selectedNum < 0) {
+					System.out.println("다시 입력하세요");
+					continue;
+				}
+				return selectedNum;
+			}catch(Exception e) {
+				System.out.println("------------------------------------------");
+				System.out.printf("오류 발생: %s\n", e.getClass().getName());
+				System.out.println("------------------------------------------");
+			}
+		}
+	}
+	private void addPlaceDetailMapValue() {
+		Map<String, String> placeMenu = new HashMap<>();
+		placeMenu.put("장소 목록", "/place/list");
+		placeMenu.put("장소 추가", "/place/add");
+		placeMenu.put("장소 삭제", "/place/delete");
+		placeMenu.put("이전 메뉴", "0");
+		controlMenu.put("지도관리", placeMenu);
+	}
+
+	private void addThemeDetailMapValue() {
+		Map<String, String> themeMenu = new HashMap<>();
+		themeMenu.put("테마 수정", "/myMap/update");
+		themeMenu.put("이전 메뉴", "0");
+		controlMenu.put("테마관리", themeMenu);
+	}
 }
