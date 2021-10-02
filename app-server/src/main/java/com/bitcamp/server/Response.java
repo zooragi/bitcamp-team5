40c5ba71d3f47709ceb5afff48bc8088257426a9
eldehd9898 <eldehd9898@gmail.com>
2021-10-03 01:39:16 +0900

    feat : Implement Response class

---
+++ app-server/src/main/java/com/bitcamp/server/Response.java
@@ -0,0 +1,33 @@
+package com.bitcamp.server;
+
+public class Response {
+	public static final String SUCCESS = "success";
+	public static final String FAIL = "fail";
+	
+	String status;
+	Object value;
+	
+	public Response() {};
+	
+	public Response(String status, Object value) {
+		this.status = status;
+		this.value = value;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Object getValue() {
+		return value;
+	}
+
+	public void setValue(Object value) {
+		this.value = value;
+	}
+	
+}
