ebe543012dc6a0402661598950fe69ab031f0ef8
eldehd9898 <eldehd9898@gmail.com>
2021-10-02 14:33:20 +0900

    feat : Add client RequestAgent

---
+++ app-client/src/main/java/com/bitcamp/request/RequestAgent.java
@@ -0,0 +1,58 @@
+package com.bitcamp.request;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import com.google.gson.Gson;
+
+public class RequestAgent implements AutoCloseable{
+	
+	public static final String SUCCESS = "success";
+	public static final String FAIL = "fail";
+	
+	Socket socket;
+	PrintWriter out;
+	BufferedReader in;
+	
+	String status;
+	String jsonData;
+	
+	public RequestAgent(String ip, int port) throws Exception{
+		socket = new Socket(ip,port);
+		out = new PrintWriter(socket.getOutputStream());
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	}
+	
+	public void request(String command, Object value) throws Exception{
+		out.println(command);
+		out.flush();
+		
+		if(value != null) {
+			out.println(new Gson().toJson(value));
+		} else {
+			out.println();
+		}
+		out.flush();
+		
+		status = in.readLine();
+		jsonData = in.readLine();
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public <T> T getObject(Class<T> type) {
+		return new Gson().fromJson(jsonData, type);
+	}
+	
+	@Override
+	public void close() {
+		try { out.close();} catch (Exception e) {}
+		try { in.close();} catch (Exception e) {}
+		try { socket.close();} catch (Exception e) {}
+	}
+	
+}
