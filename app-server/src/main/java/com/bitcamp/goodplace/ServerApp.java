865713596fce5e65bcad5656601ca6dc0d724d7d
eldehd9898 <eldehd9898@gmail.com>
2021-10-03 14:38:48 +0900

    feat : Completed the class configuration required for communication.

--- app-server/src/main/java/com/bitcamp/goodplace/ServerApp.java
+++ app-server/src/main/java/com/bitcamp/goodplace/ServerApp.java
@@ -1,5 +1,25 @@
 package com.bitcamp.goodplace;
 
-public class ServerApp {
+import java.net.ServerSocket;
+import java.net.Socket;
 
+import com.bitcamp.server.RequestProcessor;
+
+public class ServerApp {
+	public static void main(String[] args) throws Exception{
+    System.out.println("[PMS 서버]");
+
+    System.out.println("서버 실행중");
+    ServerSocket serverSocket = new ServerSocket(8888);
+
+    Socket socket = serverSocket.accept();
+    System.out.println("클라이언트가 접속했음");
+
+    RequestProcessor requestProcessor = new RequestProcessor(socket);
+    requestProcessor.service();
+    requestProcessor.close();
+
+    System.out.println("서버 종료");
+    serverSocket.close();
+	}
 }
