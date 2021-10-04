98a312bd9ef7964914d902860dbc053103933bc4
eldehd9898 <eldehd9898@gmail.com>
2021-10-05 01:50:06 +0900

    feat : Implement map parameter get value function

--- app-server/src/main/java/com/bitcamp/server/Request.java
+++ app-server/src/main/java/com/bitcamp/server/Request.java
@@ -21,5 +21,18 @@ public class Request {
 	public <T> T getObject(Class<T> type) {
 		return new Gson().fromJson(jsonData, type);
 	}
+	
+  @SuppressWarnings("unchecked")
+  public String getParameter(String name) {
+    try {
+      if (params == null) {
+        params = new Gson().fromJson(jsonData, Map.class);
+      }
+      return params.get(name);
+
+    } catch (Exception e) {
+      return null;
+    }
+  }
 
 }
