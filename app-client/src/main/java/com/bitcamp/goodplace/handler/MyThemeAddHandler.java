2499b071fb2094490e50e748477c6ab047f5021f
eldehd9898 <eldehd9898@gmail.com>
2021-10-05 23:39:25 +0900

    feat : Implement all theme storage functions.

--- app-client/src/main/java/com/bitcamp/goodplace/handler/MyThemeAddHandler.java
+++ app-client/src/main/java/com/bitcamp/goodplace/handler/MyThemeAddHandler.java
@@ -1,90 +1,89 @@
 package com.bitcamp.goodplace.handler;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import com.bitcamp.goodplace.domain.Theme;
-import com.bitcamp.goodplace.domain.User;
 import com.bitcamp.request.RequestAgent;
 import com.bitcamp.util.Prompt;
 
 public class MyThemeAddHandler implements Command {
 
-  RequestAgent requestAgent;
-  
-  public MyThemeAddHandler(RequestAgent requestAgent) {
-  	this.requestAgent = requestAgent;
-  }
+	RequestAgent requestAgent;
 
-  public void execute(CommandRequest request) throws Exception{
-    Theme theme = new Theme();
-    System.out.println();
-    System.out.println("[나의 테마 등록하기]");
-    
-    requestAgent.request("user.theme.list", AuthLoginHandler.getLoginUser().getNickName());
-    ArrayList<Theme> themeList = new ArrayList<>(requestAgent.getObjects(Theme.class));
-    
-    if(themeList.size() == 0) {
-      theme.setNo(1);
-    } else {
-      int themeNo = themeList.get(themeList.size()-1).getNo();
-      theme.setNo(++themeNo);
-    }
+	public MyThemeAddHandler(RequestAgent requestAgent) {
+		this.requestAgent = requestAgent;
+	}
 
-    theme.setTitle(Prompt.inputString("테마 이름(취소 : 엔터) > "));
+	public void execute(CommandRequest request) throws Exception {
+		Theme theme = new Theme();
+		System.out.println();
+		System.out.println("[나의 테마 등록하기]");
 
-    if(theme.getTitle().equals("")) {
-      System.out.println("나의 테마 등록 취소!");
-      return;
-    }
+		requestAgent.request("user.theme.list", AuthLoginHandler.getLoginUser().getNickName());
+		ArrayList<Theme> themeList = new ArrayList<>(requestAgent.getObjects(Theme.class));
 
-    int categoryNum;
-    List<String> categories = new ArrayList<>();
-    categories.add("식당");
-    categories.add("카페");
-    categories.add("관광명소");
-    categories.add("기타");
-    while (true) {
-      int index = 1;
-      for(String category : categories) {
-        System.out.printf("%d. %s ",index++,category);
-      }
-      System.out.println();
-      categoryNum = Prompt.inputInt("카테고리 번호 > ");
-      if(categoryNum > categories.size() || categoryNum < 0) {
-        System.out.println("잘못된 번호!");
-        continue;
-      }
-      System.out.println();
-      break;
-    }
-    theme.setCategory(categories.get(categoryNum-1));
+		if (themeList.size() == 0) {
+			theme.setNo(1);
+		} else {
+			int themeNo = themeList.get(themeList.size() - 1).getNo();
+			theme.setNo(++themeNo);
+		}
 
-    while (true) {
-      String input = Prompt.inputString("해시 태그(완료: 엔터) > ");
-      if (input.length() == 0)
-        break;
+		theme.setTitle(Prompt.inputString("테마 이름(취소 : 엔터) > "));
 
-      theme.getHashtags().add(input);
-    }
+		if (theme.getTitle().equals("")) {
+			System.out.println("나의 테마 등록 취소!");
+			return;
+		}
 
-    String publicOption = Prompt.inputString("공개 설정(Y/n) > ");
-    if (publicOption.equalsIgnoreCase("y") || publicOption.equals("") ) {
-      theme.setPublic(true);
-    }
+		int categoryNum;
+		List<String> categories = new ArrayList<>();
+		categories.add("식당");
+		categories.add("카페");
+		categories.add("관광명소");
+		categories.add("기타");
+		while (true) {
+			int index = 1;
+			for (String category : categories) {
+				System.out.printf("%d. %s ", index++, category);
+			}
+			System.out.println();
+			categoryNum = Prompt.inputInt("카테고리 번호 > ");
+			if (categoryNum > categories.size() || categoryNum < 0) {
+				System.out.println("잘못된 번호!");
+				continue;
+			}
+			System.out.println();
+			break;
+		}
+		theme.setCategory(categories.get(categoryNum - 1));
 
-    theme.setThemeOwnerName(AuthLoginHandler.getLoginUser().getNickName());
-    requestAgent.request("user.theme.insert", theme);
-    
-    if(requestAgent.getStatus().equals(RequestAgent.SUCCESS)) {
-    	System.out.println("테마 등록 완료!");
-    } else if(requestAgent.getStatus().equals(RequestAgent.FAIL)){
-    	System.out.println("테마 등록 실패!");
-    	return;
-    }
-    
-    
-    
-  }
+		while (true) {
+			String input = Prompt.inputString("해시 태그(완료: 엔터) > ");
+			if (input.length() == 0)
+				break;
+
+			theme.getHashtags().add(input);
+		}
+
+		String publicOption = Prompt.inputString("공개 설정(Y/n) > ");
+		if (publicOption.equalsIgnoreCase("y") || publicOption.equals("")) {
+			theme.setPublic(true);
+		}
+
+		theme.setThemeOwnerName(AuthLoginHandler.getLoginUser().getNickName());
+		requestAgent.request("user.theme.insert", theme);
+		requestAgent.request("theme.insert", theme);
+
+		if (requestAgent.getStatus().equals(RequestAgent.SUCCESS)) {
+			System.out.println("테마 등록 완료!");
+		} else if (requestAgent.getStatus().equals(RequestAgent.FAIL)) {
+			System.out.println("테마 등록 실패!");
+			return;
+		}
+
+	}
 
 }
