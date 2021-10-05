82327027def993f4e374268a63ac23059a8582a8
eldehd9898 <eldehd9898@gmail.com>
2021-10-05 23:33:07 +0900

    feat : Implemented the logout function.

---
+++ app-client/src/main/java/com/bitcamp/goodplace/handler/AuthLogoutHandler.java
@@ -0,0 +1,34 @@
+package com.bitcamp.goodplace.handler;
+
+import java.util.HashMap;
+import java.util.List;
+import com.bitcamp.context.UserContextListener;
+import com.bitcamp.menu.Menu;
+
+public class AuthLogoutHandler implements Command{
+
+  List<UserContextListener> userListeners;
+
+  public AuthLogoutHandler(List<UserContextListener> userListeners) {
+    this.userListeners = userListeners;
+  }
+
+  public void execute(CommandRequest request) {
+    System.out.println("[로그아웃]");
+
+    AuthLoginHandler.loginUser = null;
+    AuthLoginHandler.useAccessLevel = Menu.ACCESS_LOGOUT;
+    notifyOnLogout();
+
+  }
+
+  private void notifyOnLogout() {
+    HashMap<String, Object> params = new HashMap<>();
+
+    for (UserContextListener listener : userListeners) {
+      listener.contextLogout(params);
+    }
+  }
+
+
+}
