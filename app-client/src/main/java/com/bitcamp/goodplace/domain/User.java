74da24b006cbf78d72292de232446ec2c2c7d0ee
eldehd9898 <eldehd9898@gmail.com>
2021-10-08 17:57:23 +0900

    fix : Delete bookmarks and theme lists

--- app-client/src/main/java/com/bitcamp/goodplace/domain/User.java
+++ app-client/src/main/java/com/bitcamp/goodplace/domain/User.java
@@ -14,19 +14,14 @@ public class User implements Comparable<User> {
   private int reportedCount = 0;
   private int warningCount = 0;
 
-  private List<User> likedUsers = new ArrayList<>(); //팔로우
-  private List<Theme> themeList = new ArrayList<>();
-  private List<Theme> likedThemes = new ArrayList<>(); // 북마크
-
-
-
+  private List<User> likedUsers = new ArrayList<>(); //팔로우 
 
   @Override
   public String toString() {
     return "User [no=" + no + ", email=" + email + ", password=" + password + ", nickName="
         + nickName + ", registeredDate=" + registeredDate + ", viewCount=" + viewCount
         + ", reportedCount=" + reportedCount + ", warningCount=" + warningCount + ", likedUsers="
-        + likedUsers + ", themeList=" + themeList + ", likedThemes=" + likedThemes + "]";
+        + likedUsers + ", ]";
   }
 
 
@@ -38,10 +33,6 @@ public class User implements Comparable<User> {
   public void setReportedCount(int reportedCount) {
     this.reportedCount = reportedCount;
   }
-
-
-
-
 
   public int getNo() {
     return no;
@@ -116,28 +107,6 @@ public class User implements Comparable<User> {
   public void setLikedUsers(List<User> likedUsers) {
     this.likedUsers = likedUsers;
   }
-
-
-  public List<Theme> getThemeList() {
-    return themeList;
-  }
-
-
-
-  public void setThemeList(List<Theme> themeList) {
-    this.themeList = themeList;
-  }
-
-
-  public List<Theme> getLikedThemes() {
-    return likedThemes;
-  }
-
-
-  public void setLikedThemes(List<Theme> likedThemes) {
-    this.likedThemes = likedThemes;
-  }
-
 
   @Override
   public int compareTo(User user) {
