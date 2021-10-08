8217be50e87a4f937b7fe926cdbde23340f8aeaf
eldehd9898 <eldehd9898@gmail.com>
2021-10-08 17:59:58 +0900

    feat : Add themeTable class

---
+++ app-server/src/main/java/com/bitcamp/goodplace/Table/ThemeTable.java
@@ -0,0 +1,105 @@
+package com.bitcamp.goodplace.Table;
+
+import com.bitcamp.goodplace.domain.Theme;
+import com.bitcamp.server.Request;
+import com.bitcamp.server.Response;
+
+public class ThemeTable extends JsonDataTable<Theme> {
+
+	public ThemeTable() {
+		super("theme.json", Theme.class);
+	}
+
+	public void execute(Request request, Response response) {
+		switch (request.getCommand()) {
+		case "theme.insert":
+			themeInsert(request, response);
+			break;
+		case "theme.list":
+			themeList(request, response);
+			break;
+		case "theme.update":
+			themeUpdate(request, response);
+			break;
+		case "theme.delete":
+			themeDelete(request, response);
+			break;
+		default:
+			response.setStatus(Response.FAIL);
+			response.setValue("해당 명령을 지원하지 않습니다.");
+
+		}
+	}
+
+	private void themeDelete(Request request, Response response) {
+		String deleteThemeTitle = request.getObject(String.class);
+		Theme deleteTheme = findByTitle(deleteThemeTitle);
+		list.remove(deleteTheme);
+		response.setStatus(Response.SUCCESS);
+		response.setValue(deleteTheme);
+	}
+
+	private void themeUpdate(Request request, Response response) {
+		Theme theme = request.getObject(Theme.class);
+		list.set(indexOf(theme), theme);
+		response.setStatus(Response.SUCCESS);
+	}
+
+	private void themeList(Request request, Response response) {
+		response.setValue(list);
+		response.setStatus(Response.SUCCESS);
+	}
+
+	private void themeInsert(Request request, Response response) {
+		Theme theme = request.getObject(Theme.class);
+		list.add(theme);
+		response.setStatus(Response.SUCCESS);
+		response.setValue(list);
+	}
+
+	private int indexOf(Theme theme) {
+		int i = 0;
+		for (Theme t : list) {
+			if (t.getNo() == theme.getNo()) {
+				return i;
+			}
+			i++;
+		}
+		return -1;
+	}
+
+//	private User findByThemeNo(int no) {
+//		for(User user : list) {
+//			for(Theme theme : user.getThemeList()) {
+//				if(theme.getNo() == no) {
+//					return user;
+//				}
+//			}
+//		}
+//		return null;
+//	}
+//	
+	private Theme findByTitle(String title) {
+		for (Theme theme : list) {
+			if (theme.getTitle().equals(title)) {
+				return theme;
+			}
+		}
+
+		return null;
+	}
+//	
+//	private int indexOfTheme(Theme theme) {
+//		for(User user : list) {
+//			int i = 0;
+//			for(Theme t : user.getThemeList()) {
+//				if(t.getNo() == theme.getNo()) {
+//					return i;
+//				}
+//				i++;
+//				
+//			}
+//		}
+//		return -1;
+//	}
+}
