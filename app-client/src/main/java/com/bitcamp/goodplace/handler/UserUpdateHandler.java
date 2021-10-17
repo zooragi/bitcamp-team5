ecb22c700404ea427e30c3fa733f908aa2b67a33
eldehd9898 <eldehd9898@gmail.com>
2021-10-17 09:56:24 +0900

    feat : Add membership update function.

---
+++ app-client/src/main/java/com/bitcamp/goodplace/handler/UserUpdateHandler.java
@@ -0,0 +1,36 @@
+package com.bitcamp.goodplace.handler;
+
+import com.bitcamp.goodplace.dao.UserDao;
+import com.bitcamp.goodplace.domain.User;
+import com.bitcamp.util.Prompt;
+
+public class UserUpdateHandler implements Command {
+
+  UserDao userDao;
+
+  public UserUpdateHandler(UserDao userDao) {
+    this.userDao = userDao;
+  }
+
+  public void execute(CommandRequest request) throws Exception {
+
+    System.out.println("[내 정보 수정하기]");
+
+    User user = (User) request.getAttribute("loginUser");
+
+    User temp = new User();
+
+    temp.setEmail(Prompt.inputString("이메일(" + user.getEmail() + ") > "));
+    temp.setPassword(Prompt.inputString("암호 > "));
+    temp.setNickName(Prompt.inputString("닉네임(" + user.getNickName() + ") > "));
+
+    String input = Prompt.inputString("수정하기(y/N) > ");
+    if (input.equalsIgnoreCase("n") || input.length() == 0) {
+      System.out.println("수정 취소!");
+      return;
+    }
+
+    userDao.update(temp);
+    System.out.println("회원 수정 완료!");
+  }
+}
