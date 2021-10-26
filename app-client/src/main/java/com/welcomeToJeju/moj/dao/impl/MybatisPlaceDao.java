dc4b604cb544f2dc3d7941ad5ee6a4d02b77620e
eldehd9898 <eldehd9898@gmail.com>
2021-10-26 20:47:45 +0900

    fix : Fixing bugs that can't add locations.

--- app-client/src/main/java/com/welcomeToJeju/moj/dao/impl/MybatisPlaceDao.java
+++ app-client/src/main/java/com/welcomeToJeju/moj/dao/impl/MybatisPlaceDao.java
@@ -1,14 +1,13 @@
 package com.welcomeToJeju.moj.dao.impl;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 
 import com.welcomeToJeju.moj.dao.PlaceDao;
+import com.welcomeToJeju.moj.domain.Comment;
+import com.welcomeToJeju.moj.domain.Photo;
 import com.welcomeToJeju.moj.domain.Place;
 
 public class MybatisPlaceDao implements PlaceDao{
@@ -20,11 +19,37 @@ public class MybatisPlaceDao implements PlaceDao{
 	
 	@Override
 	public void insert(Place place) throws Exception {
+		sqlSession.insert("PlaceMapper.insert",place);
+		HashMap<String,Object> param1 = new HashMap<>();
+		HashMap<String,Object> param2 = new HashMap<>();
+		
+		for(Comment comment : place.getComments()) {
+			param1.put("placeNo", place.getNo());
+			param1.put("userNo", place.getOwner().getNo());
+			param1.put("comment", comment.getComment());
+			sqlSession.insert("PlaceMapper.commentInsert",param1);
+		}
+		
+		
+		for(Photo photo : place.getPhotos()) {
+			param2.put("placeNo", place.getNo());
+			param2.put("userNo", place.getOwner().getNo());
+			param2.put("filePath", photo.getFilePath());
+			sqlSession.insert("PlaceMapper.photoInsert",param2);
+		}
+		
+		
+		sqlSession.commit();
 	}
 	
 	@Override
-	public List<Place> findByThemeNo(int themeNo) throws Exception{
+	public List<Place> findByThemeNo(int themeNo) throws Exception {
 		return sqlSession.selectList("PlaceMapper.findByThemeNo",themeNo);
 	}
 
+	public void delete(Place place) throws Exception {
+		sqlSession.delete("PlaceMapper.deletePhoto", place.getNo());
+		sqlSession.delete("PlaceMapper.deleteComment", place.getNo());
+		sqlSession.delete("PlaceMapper.delete", place.getNo());
+	}
 }
